df65fed8788420df17a7146d2c5e2ea86044d357
MIF50 <mohamedibrahim1223@gmail.com>
2020-08-07 18:14:54 +0300

    create card view.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,9 +8,37 @@
 
 import SwiftUI
 
+// create cardView
+struct CardView: View {
+    
+    @State var isFaceUp : Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+
+            }
+        }
+        .foregroundColor(.orange)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ForEach(0..<7){ row in
+                HStack {
+                    ForEach(0..<4){ column in
+                        CardView(isFaceUp: (row) % 2 == 0)
+                    }
+                }
+            }
+        }.padding()
     }
 }
 
